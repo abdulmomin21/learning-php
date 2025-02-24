814e26d05de8fe76080f4160c939ae8a9943ab50
abdulmomin21 <mi8913970@gmail.com>
2025-02-24 13:36:32 +0600

    PHP Fundamentals Bangla Tutorial Part-03 (PHP Syntax)

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
         <title>php syntax</title>
         <style>
             .maincontent{min-height: 400px;padding: 20px;}
-            .phpcoding{width:900px;margin: auto;background: #ddd;}
+            .phpcoding{width:900px;margin: auto;background: <?php echo"#ddd"?>;}
             .headeroption, .footeroption{background: red;color: #fff;text-align:center;padding: 20px; margin: 0;}
         </style>
     </head>
@@ -21,7 +21,7 @@
                 ?>
             </section>
             <section class="footeroption">
-                <h2>www.google.com</h2>
+                <h2><?php echo"www.google.com"?></h2>
             </section>
 
 
