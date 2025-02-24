6c16900507f1c7ce0265ab7b6f80f7585db887e6
abdulmomin21 <mi8913970@gmail.com>
2025-02-24 13:24:12 +0600

    PHP Fundamentals Bangla Tutorial Part-03 (PHP Syntax)

--- index.php
+++ index.php
@@ -2,13 +2,26 @@
 <html>
     <head>
         <title>php syntax</title>
+        <style>
+            .maincontent{min-height: 400px;padding: 20px;}
+            .phpcoding{width:900px;margin: auto;background: #ddd;}
+            .headeroption, .footeroption{background: red;color: #fff;text-align:center;padding: 20px; margin: 0;}
+        </style>
     </head>
     <body>
         <div class="phpcoding">
+            <section class="headeroption">
+                <h2>php fundamantal training</h2>
+
+            </section>
+
             <section class="maincontent">
                 <?php
                 echo"i love php";
                 ?>
+            </section>
+            <section class="footeroption">
+                <h2>www.google.com</h2>
             </section>
 
 
