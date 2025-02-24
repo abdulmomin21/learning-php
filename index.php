21afad6add5b21168f221bb8fd4b01472fb911c7
abdulmomin21 <mi8913970@gmail.com>
2025-02-24 12:54:23 +0600

    PHP Fundamentals Bangla Tutorial Part-03 (PHP Syntax)

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>php syntax</title>
+    </head>
+    <body>
+        <div class="phpcoding">
+            <section class="maincontent">
+                <?php
+                echo"i love php";
+                ?>
+            </section>
+
+
+        </div>
+    </body>
+</html>
