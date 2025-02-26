0672479ce4fb512010405e8e5d4d4980cbbcf87f
abdulmomin21 <mi8913970@gmail.com>
2025-02-26 12:32:46 +0600

    PHP Fundamentals Bangla Tutorial Part-16 (Conditional Statements)

---
+++ tutorial17.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>php syntax</title>
+        <style>
+            .maincontent{min-height: 400px;padding: 20px;}
+            .phpcoding{width:900px;margin: auto;background: <?php echo"#ddd"?>;}
+            .headeroption, .footeroption{background: red;color: #fff;text-align:center;padding: 20px; margin: 0;}
+        </style>
+    </head>
+    <body>
+        <div class="phpcoding">
+            <section class="headeroption">
+                <h2>php fundamantal training</h2>
+
+            </section>
+
+            <section class="maincontent">
+                <br>
+                php conditional statements
+                <hr>
+
+                <?php
+                $x =10;
+                if($x > 54){
+                    echo "x is big";
+
+                }else if($x > 5){
+                    echo "x is greater then 5";
+                }else{
+                    echo "x is small";
+                }
+                ?>
+            </section>
+            <section class="footeroption">
+                <h2><?php echo"www.google.com //momin"?></h2>
+            </section>
+
+            
+
+
+        </div>
+    </body>
+</html>
