6171a0ed9ed079750aaff5f74798850d8e2b4f67
abdulmomin21 <mi8913970@gmail.com>
2025-03-05 17:09:34 +0600

    PHP Array Functions Bangla Tutorial Part-07 (array_diff_assoc)

---
+++ tutorial54.php
@@ -0,0 +1,71 @@
+<?php 
+session_start();
+//eta diye cookie delete kora hoy
+setcookie('visited', "", time() -3600);
+?>
+<!DOCTYPE html>
+<html> 
+    <head>
+        <title>php syntax</title>
+        <style>
+            .maincontent{min-height: 400px;padding: 20px;}
+            .phpcoding{width:900px;margin: auto;background: <?php echo"#ddd"?>;}
+            .headeroption, .footeroption{background: red;color: #fff;text-align:center;padding: 20px; margin: 0;}
+            input[type= "text"]{width:250px}
+        </style>
+    </head>
+    <body>
+        <div class="phpcoding">
+            <section class="headeroption">
+                <h2>php fundamantal training</h2>
+
+            </section>
+
+            <section class="maincontent">
+                <hr>
+                array diff
+                <span style="float:right">
+                <?php
+                date_default_timezone_set('asia/dhaka');
+                echo "bangladesh time is ".date("h:i:sa");
+                ?>
+
+                </span>
+                <hr>
+               <?php
+               $array_one = array (
+                "a" => "red",
+                "b" => "green",
+                "c" => "blue",
+                "d" => "yellow"
+               );
+               $array_two = array (
+                "e" => "red",
+                "f" => "green",
+                "g" => "blue"
+               );
+               $array_three = array (
+                "h" => "red",
+                "b" => "green",
+                "g" => "blue",
+                "j" => "yellow"
+               );
+               $differ = array_diff_assoc($array_one, $array_two, $array_three);
+
+               print("<pre>");
+               print_r($differ);
+               print("</pre>");
+
+
+               ?>
+            </section>
+            <section class="footeroption">
+                <h2><?php echo"www.google.com //momin"?></h2>
+            </section>
+
+            
+
+
+        </div>
+    </body>
+</html>
