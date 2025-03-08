1d055e19cacf0a19b827b66cc5c57eb94c2c4a1c
abdulmomin21 <mi8913970@gmail.com>
2025-03-08 14:48:37 +0600

    PHP Array Functions Bangla Tutorial Part-12 (array_key_exists)

---
+++ tutorial59.php
@@ -0,0 +1,64 @@
+<?php 
+session_start();
+//eta diye cookie delete kora hoy
+setcookie('visited', "", time() -3600);
+?>
+<!DOCTYPE html>
+<html> 
+    <head>
+        <title>php syntax</title>
+        <style>
+            .maincontent{min-height: 400px;padding: 20px;}
+            .phpcoding{width:900px;margin: auto;background: <?php echo"#ddd"?>;}
+            .headeroption, .footeroption{background: red;color: #fff;text-align:center;padding: 20px; margin: 0;}
+            input[type= "text"]{width:250px}
+        </style>
+    </head>
+    <body>
+        <div class="phpcoding">
+            <section class="headeroption">
+                <h2>php fundamantal training</h2>
+
+            </section>
+
+            <section class="maincontent">
+                <hr>
+                array_key_exists
+                <span style="float:right">
+                <?php
+                date_default_timezone_set('asia/dhaka');
+                echo "bangladesh time is ".date("h:i:sa");
+                ?>
+
+                </span>
+                <hr>
+              <?php
+              /*
+              $arr = array(
+                "name" => "abdul momoin",
+                "age"  => "23"
+              );
+              if(array_key_exists("name", $arr)){
+                echo "key already exists";
+              }else {
+                echo "key does not exists.";
+              } */
+              $arr = array("name", "age");
+              if(array_key_exists("2", $arr)){
+                echo "key already exists";
+              }else {
+                echo "key does not exists.";
+              }
+
+              ?>
+            </section>
+            <section class="footeroption">
+                <h2><?php echo"www.google.com //momin"?></h2>
+            </section>
+
+            
+
+
+        </div>
+    </body>
+</html>
