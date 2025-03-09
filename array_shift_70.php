ab5161dd56bb5565fe0e7938aca1bca4cc0fd91b
abdulmomin21 <mi8913970@gmail.com>
2025-03-09 16:25:17 +0600

    PHP Array Functions Bangla Tutorial Part-23 (array_shift)

---
+++ array_shift_70.php
@@ -0,0 +1,63 @@
+<?php 
+session_start();
+//eta diye cookie delete kora hoy
+setcookie('visited', "", time() -3600);
+?>
+<!DOCTYPE html>
+<html> 
+    <head>
+        <title>php syntax</title>
+        <style>
+            .maincontent{min-height: 400px;padding: 20px;}
+            .phpcoding{width:900px;margin: auto;background: <?php echo"#ddd"?>;}
+            .headeroption, .footeroption{background: red;color: #fff;text-align:center;padding: 20px; margin: 0;}
+        </style>
+    </head>
+    <body>
+        <div class="phpcoding">
+            <section class="headeroption">
+                <h2>php fundamantal training</h2>
+
+            </section>
+
+            <section class="maincontent">
+                <hr>
+                array_shift
+                <span style="float:right">
+                <?php
+                date_default_timezone_set('asia/dhaka');
+                echo "bangladesh time is ".date("h:i:sa");
+                ?>
+
+                </span>
+                <hr>
+               <?php
+              /* $color_one = array(
+                "a" => "red",
+                "b" => "green",
+                "c" => "blue",
+                "d" => "yellow" */
+                $color_one = array(
+                    "0" => "red",
+                    "2" => "green",
+                    "5" => "blue",
+                    "6" => "yellow"
+                    /*numeric key dile eta default hisebe ney */
+               );
+               array_shift($color_one);
+
+               print("<pre>");
+               print_r($color_one);
+               print("</pre>");
+               ?>
+            </section>
+            <section class="footeroption">
+                <h2><?php echo"www.google.com //momin"?></h2>
+            </section>
+
+            
+
+
+        </div>
+    </body>
+</html>
