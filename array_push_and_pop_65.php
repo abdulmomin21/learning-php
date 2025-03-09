35a05acdca91b63558349e3b6af036972b2a54c2
abdulmomin21 <mi8913970@gmail.com>
2025-03-09 13:20:54 +0600

    PHP Array Functions Bangla Tutorial Part-18 (array_pop & array_push)

---
+++ array_push_and_pop_65.php
@@ -0,0 +1,54 @@
+<?php 
+session_start();
+//eta diye cookie delete kora hoy
+setcookie('visited', "", time() -3600);
+?>
+<!DOCTYPE html>
+<html> 
+    <head>
+        <title>php syntax</title>
+        <style>
+            .maincontent{min-height: 400px;padding: 20px;}
+            .phpcoding{width:900px;margin: auto;background: <?php echo"#ddd"?>;}
+            .headeroption, .footeroption{background: red;color: #fff;text-align:center;padding: 20px; margin: 0;}
+            input[type= "text"]{width:250px}
+        </style>
+    </head>
+    <body>
+        <div class="phpcoding">
+            <section class="headeroption">
+                <h2>php fundamantal training</h2>
+
+            </section>
+
+            <section class="maincontent">
+                <hr>
+                array_pop & array_push
+                <span style="float:right">
+                <?php
+                date_default_timezone_set('asia/dhaka');
+                echo "bangladesh time is ".date("h:i:sa");
+                ?>
+
+                </span>
+                <hr>
+                <?php
+             $arr = array("red","blue");
+            /* array_pop($arr); */
+            array_push($arr, "yellow","sky blue");
+
+             print("<pre>");
+             print_r($arr);
+             print("</pre>");
+              ?>
+            </section>
+            <section class="footeroption">
+                <h2><?php echo"www.google.com //momin"?></h2>
+            </section>
+
+            
+
+
+        </div>
+    </body>
+</html>
