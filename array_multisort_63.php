d9f2b4889bdf0fa4021112bc2877afdc3d2bc548
abdulmomin21 <mi8913970@gmail.com>
2025-03-09 12:46:50 +0600

    PHP Array Functions Bangla Tutorial Part-16 (array_multisort)

---
+++ array_multisort_63.php
@@ -0,0 +1,67 @@
+<?php 
+session_start();
+//eta diye cookie delete kora hoy
+setcookie('visited', "", time() -3600);
+?>
+<!DOCTYPE html>
+<html> 
+    <head>
+        <title>php syntax</title>
+        <style>
+            .maincontent{min-height: 400px;padding: 20px;}
+            .phpcoding{width:900px;margin: auto;background: <?php echo"#ddd"?>;}
+            .headeroption, .footeroption{background: red;color: #fff;text-align:center;padding: 20px; margin: 0;}
+            input[type= "text"]{width:250px}
+        </style>
+    </head>
+    <body>
+        <div class="phpcoding">
+            <section class="headeroption">
+                <h2>php fundamantal training</h2>
+
+            </section>
+
+            <section class="maincontent">
+                <hr>
+                array_merge
+                <span style="float:right">
+                <?php
+                date_default_timezone_set('asia/dhaka');
+                echo "bangladesh time is ".date("h:i:sa");
+                ?>
+
+                </span>
+                <hr>
+               <?php
+            /*   $arr_one = array("dog", "cat");
+               array_multisort($arr_one);
+
+               print("<pre>");
+               print_r($arr_one);
+               print("</pre>"); */
+
+               $arr_one = array ("dog", "cat");
+               $arr_two = array("lion", "tiger");
+
+               array_multisort($arr_one,SORT_ASC,$arr_two,SORT_ASC);
+
+               print("<pre>");
+               print_r($arr_one);
+               print("</pre>");
+
+               print("<pre>");
+               print_r($arr_two);
+               print("</pre>");
+               ?>
+              
+            </section>
+            <section class="footeroption">
+                <h2><?php echo"www.google.com //momin"?></h2>
+            </section>
+
+            
+
+
+        </div>
+    </body>
+</html>
