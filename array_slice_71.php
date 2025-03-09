74e0fd872cd932e51bafc0e50ac78c3de1ec1ab1
abdulmomin21 <mi8913970@gmail.com>
2025-03-09 17:26:29 +0600

    PHP Array Functions Bangla Tutorial Part-24 (array_slice)

---
+++ array_slice_71.php
@@ -0,0 +1,52 @@
+<?php 
+session_start();
+//eta diye cookie delete kora hoy
+setcookie('visited', "", time() -3600);
+?>
+<!DOCTYPE html>
+<html> 
+    <head>
+        <title>php syntax</title>
+        <style>
+            .maincontent{min-height: 400px;padding: 20px;}
+            .phpcoding{width:900px;margin: auto;background: <?php echo"#ddd"?>;}
+            .headeroption, .footeroption{background: red;color: #fff;text-align:center;padding: 20px; margin: 0;}
+        </style>
+    </head>
+    <body>
+        <div class="phpcoding">
+            <section class="headeroption">
+                <h2>php fundamantal training</h2>
+
+            </section>
+
+            <section class="maincontent">
+                <hr>
+                array_slice
+                <span style="float:right">
+                <?php
+                date_default_timezone_set('asia/dhaka');
+                echo "bangladesh time is ".date("h:i:sa");
+                ?>
+
+                </span>
+                <hr>
+               <?php
+               $color_one = array("red", "green", "blue", "yellow", "skyblue");
+               $result = array_slice($color_one,1,4);
+
+               print("<pre>");
+               print_r($result);
+               print("</pre>");
+               ?>
+            </section>
+            <section class="footeroption">
+                <h2><?php echo"www.google.com //momin"?></h2>
+            </section>
+
+            
+
+
+        </div>
+    </body>
+</html>
