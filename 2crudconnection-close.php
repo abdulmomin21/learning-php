69c7d2b8bf3d1759d327f2249387da5a477b65cf
abdulmomin21 <mi8913970@gmail.com>
2025-04-07 12:47:59 +0600

    crud practice connection

--- 2crudconnection-close.php
+++ 2crudconnection-close.php
@@ -3,12 +3,11 @@ $hostname = 'localhost';
 $username = 'root';
 $password = '';
 
-$conn = new mysqli($hostname, $username, $password);
+$coon = new mysqli($hostname, $username, $password);
 
-    if($conn->connect_error){
-        echo $conn->connect_error;
+    if($coon->connect_error){
+        echo $coon->connect_error;
     }else{
         echo "connected";
     }
-$conn->close();
-?>
+$coon->close();
