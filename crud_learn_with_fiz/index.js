20394be80064c8f026824074f6b5222cd8562bba
abdulmomin21 <mi8913970@gmail.com>
2025-04-26 19:18:56 +0600

    crud learn with fiz

---
+++ crud_learn_with_fiz/index.js
@@ -0,0 +1,8 @@
+function onsaveupdate()
+{
+    var user_name = $("#user_name").val();
+    var user_email = $("#user_email").val();
+    var user_dept = $("#user_dept").val();
+    var user_section = $("#user_section").val();
+    
+}
---
+++ crud_learn_with_fiz/index.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <h1 class="bg-dark p-2 text-center text-white">php oop crud using bootstrap modal and ajax</h1>
+
+    <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  add student 
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">student info</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <input type="text" class="form-control" placeholder="enter name" id="user_name">
+        <input type="email" class="form-control" placeholder="enter email" id="user_email">
+        <input type="text" class="form-control" placeholder="enter dept" id="user_dept">
+        <select name="" id="user_section" class="form-control">
+            <option value="0">select section</option>
+            <option value="1">A</option>
+            <option value="2">B</option>
+            <option value="3">C</option>
+        </select>
+        
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary" onclick="onsaveupdate()">Save</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js"><script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="index.js"></script>
+</body>
+
+</html>
