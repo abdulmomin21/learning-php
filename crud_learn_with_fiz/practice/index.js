26bb10629f113e1098292cdace0183192859ddc9
abdulmomin21 <mi8913970@gmail.com>
2025-04-28 12:43:19 +0600

    practice

---
+++ crud_learn_with_fiz/practice/index.js
@@ -0,0 +1,8 @@
+function OnSaveUpdate()
+{
+    var user_name = $("#user_name").val();
+    var user_email = $("#user_email").val();
+    var user_dept = $("#user_dept").val();
+    var user_section = $("#user_section").val();
+    alert(user_name+"__"+user_email+"__"+user_dept+"__"+user_section)
+}
